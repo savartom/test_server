98ea70962537f3fe71d85ecff89066c5f030bbb5
savartom <artyom.savin.2019@inbox.ru>
2024-03-09 15:19:45 +0300

    Second

--- start.sh
+++ start.sh
@@ -6,11 +6,11 @@ export PYTHONUNBUFFERED=true
 
 VIRTUALENV=./venv
 
-if [ ! -d $VIRTUALENV]; then
+if [ ! -d $VIRTUALENV ]; then
   python3 -m venv $VIRTUALENV
 fi
 
-if [! -f $VIRTUALENV/bin/pip]; then
+if [ ! -f $VIRTUALENV/bin/pip ]; then
   curl --silent --show-err --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
 
